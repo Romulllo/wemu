bd6e8a4b8b1a29b2954906b9116ff704d09c4c48
Romulo Santos <romulosantos88@hotmail.com>
2020-11-14 11:42:34 -0300

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  resources :communities do
+    resources :message
+  end
+
+  resources :profiles, only: [ :show ]
+  
+  get 'home', to: 'pages#home'
+  get 'results', to: 'pages#result'
+  
 end
