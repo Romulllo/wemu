15ebdb89ac2b2168897325976f78f4c184773cb8
Romulo Santos <romulosantos88@hotmail.com>
2020-11-20 12:31:49 -0300

    db:seed for community

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :landing ]
 
   def home
+    @community = Community.where(name: params[:query])
   end
 
   def landing
