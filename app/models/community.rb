79d1ce1345bd6f217386289fe060ab261f1918c0
Bernardo Neves <bernardosbneves@gmail.com>
2020-11-21 16:35:37 -0300

    lot of changes in the shows and new from communities

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,7 @@
 class Community < ApplicationRecord
   belongs_to :user
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_many :memberships
   has_many :users, through: :memberships
+  has_one_attached :photo
 end
