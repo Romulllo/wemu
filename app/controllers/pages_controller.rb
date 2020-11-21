e82ee4309cf8e0e28c57a3bb4b889882dd9f25a1
Romulo Santos <romulosantos88@hotmail.com>
2020-11-21 10:39:57 -0300

    git

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :landing ]
 
-  def home
-    @community = Community.where(name: params[:query])
+  def results
+    if params[:query].present?
+      @community = Community.where(name: params[:query])
+    else
+      @community = Community.all
+    end
   end
 
   def landing
