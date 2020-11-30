19153d5adf9b495a4d26bb64f33bfdbc88808c57
Romulo Santos <romulosantos88@hotmail.com>
2020-11-30 11:44:00 -0300

    dependent destroy for community

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,9 +1,9 @@
 class Community < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
 
   include PgSearch::Model
   pg_search_scope :search_by_name,
