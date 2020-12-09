bb1a6d132bee43bfaa573cc37be556f8d3e1ea44
Romulo Santos <romulosantos88@hotmail.com>
2020-12-09 18:12:16 -0300

    go to heroku

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -2,23 +2,26 @@ import 'js-autocomplete/auto-complete.css';
 import autocomplete from 'js-autocomplete';
 
 const autocompleteSearch = function() {
-  const communities = JSON.parse(document.getElementById('search-data').dataset.communities_auto)
-  const searchInput = document.getElementById('query');
-
-  if (communities && searchInput) {
-    new autocomplete({
-      selector: searchInput,
-      minChars: 1,
-      source: function(term, suggest){
+  const search = document.getElementById('search-data')
+  if (search) {
+    const communities = JSON.parse(search.dataset.communities_auto)
+    const searchInput = document.getElementById('query');
+    
+    if (communities && searchInput) {
+      new autocomplete({
+        selector: searchInput,
+        minChars: 1,
+        source: function(term, suggest){
           term = term.toLowerCase();
           const choices = communities;
           const matches = [];
           for (let i = 0; i < choices.length; i++)
-              if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+          if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
           suggest(matches);
-      },
-      onSelect: autocompleteSearch
-    });
+        },
+        onSelect: autocompleteSearch
+      });
+    }
   }
 };
 
