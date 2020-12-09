aba533f696082b08371a8d72f620fefd013bbde5
Romulo Santos <romulosantos88@hotmail.com>
2020-12-09 11:22:08 -0300

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://we-mu.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://we-mu.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
