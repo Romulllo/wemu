e68baf8296fe37c5ac4b9f1ff70655319a245afd
Romulo Santos <romulosantos88@hotmail.com>
2020-12-11 12:00:18 -0300

    validates

--- app/models/community.rb
+++ app/models/community.rb
@@ -3,7 +3,10 @@ class Community < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :memberships, dependent: :delete_all
   has_many :users, through: :memberships
+  validates :photo, presence: true
   has_one_attached :photo, dependent: :destroy
+
+
 
   include PgSearch::Model
   pg_search_scope :search_by_name,
